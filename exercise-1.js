2496af2671a35980a859f750f036bc2739dbfb03
Name <pobparis.bun@gmail.com>
2024-01-11 03:11:10 +0700

    finish mongo

---
+++ exercise-1.js
@@ -0,0 +1,9 @@
+{
+    _id: ObjectId('659e3ebcbc937050f61aa04c'),
+    size: 'medium',
+    total_price: 4627.41,
+    quantity: 12,
+    customer_name: 'Cherlyn',
+    credit_card_type: 'mastercard',
+    created_at: 2021-06-27T23:57:12.000Z
+  }
